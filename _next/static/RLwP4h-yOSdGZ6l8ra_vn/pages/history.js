7df4a15dd6f475ed806b304553b8383b01656047
KitaharaMugiro <KitaharaMugiro@users.noreply.github.com>
2020-01-23 05:48:14 +0000

    Automated deployment: Thu Jan 23 05:48:14 UTC 2020 c2ac855d3198819061b6fe6e2bd6dd25cefd6f23

--- _next/static/RLwP4h-yOSdGZ6l8ra_vn/pages/history.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{OgSG:function(n,o,i){(window.__NEXT_P=window.__NEXT_P||[]).push(["/history",function(){return i("pMUx")}])},pMUx:function(n,o,i){"use strict";i.r(o),o.default=function(){return"history"}}},[["OgSG",0]]]);
