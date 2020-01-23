4a62cd06e3bf10852d5bb9713018528d62b6c577
KitaharaMugiro <KitaharaMugiro@users.noreply.github.com>
2020-01-23 20:25:49 +0000

    Automated deployment: Thu Jan 23 20:25:49 UTC 2020 883121be3c89038781cddb0f1eed2c479fbe244a

--- _next/static/0LPhn-5WWCJzRTmXPZ4T8/pages/products/auto-trading.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[10],{NtMK:function(n,t,o){"use strict";o.r(t),t.default=function(){return"\u82e6\u52b4\u3057\u305f\u3053\u3068"}},OUda:function(n,t,o){(window.__NEXT_P=window.__NEXT_P||[]).push(["/products/auto-trading",function(){return o("NtMK")}])}},[["OUda",0]]]);
---
+++ _next/static/5LitoVZS040yi7LBJgeVV/pages/history.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{OgSG:function(n,o,i){(window.__NEXT_P=window.__NEXT_P||[]).push(["/history",function(){return i("pMUx")}])},pMUx:function(n,o,i){"use strict";i.r(o),o.default=function(){return"history"}}},[["OgSG",0]]]);
