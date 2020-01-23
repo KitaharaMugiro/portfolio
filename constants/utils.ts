ac413f1a35d50a7c13a9861bc0c9e00d011e5907
KitaharaMugiro <ngthornym@yahoo.co.jp>
2020-01-24 05:55:18 +0900

    修正

--- constants/utils.ts
+++ constants/utils.ts
@@ -1,5 +1,5 @@
 export function getPath(rawPath: string) {
-  const env = process.env.NODE_ENV;
+  const env = process.env.NODE_ENV === "production";
   console.log({ env });
   const baseUrl = env ? "/portfolio" : "";
   return baseUrl + rawPath;
